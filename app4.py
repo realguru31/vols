82ab42c6d1420fabfa748c2acd27d13544e1b7b6
realguru31 <157280424+realguru31@users.noreply.github.com>
2026-02-04 05:37:15 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -8,7 +8,7 @@ st.title("SPY 1-Minute Data")
 
 # Fetch 1-minute data for SPY
 # Symbol: SPY, Exchange: AMEX, Interval: 1 minute
-data = tv.get_hist(symbol='SPY', exchange='AMEX', interval=Interval.in_1_minute, n_bars=100)
+data = tv.get_hist(symbol='XAUUSD', exchange='OANDA', interval=Interval.in_1_minute, n_bars=100)
 
 if data is not None:
     st.subheader("Last 100 Minutes of SPY")
